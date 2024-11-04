987bf13a3227b19aaf48f5363a5d720db281bc30
SeqSEE <SeqSEE@users.noreply.github.com>
2024-11-04 17:20:38 -0500

    fix spacing?

--- init.lua
+++ init.lua
@@ -3,38 +3,38 @@ minetest.register_node("barrier:barrier", {
     drawtype = "airlike",
     tiles = {""},
     alpha = 0,
-	paramtype = "light",
+    paramtype = "light",
     light_source = 0,
     use_texture_alpha = true,
-	sunlight_propagates = true,
+    sunlight_propagates = true,
     walkable = true,
-	pointable = false,
-	buildable_to = true,
-	drop = "",
-	groups = {invisible = 1, cracky = 1, not_in_creative_inventory = 1},
+    pointable = false,
+    buildable_to = true,
+    drop = "",
+    groups = {invisible = 1, cracky = 1, not_in_creative_inventory = 1},
     on_construct = function(pos) end,
-    can_dig = function(pos, player) 
+    can_dig = function(pos, player)
         local wielded_item = player:get_wielded_item():get_name()
         return wielded_item == "barrier:barrier_item"
-     end,
+    end,
     on_blast = function() end
 })
 
 minetest.register_tool("barrier:barrier_item", {
-	description = "Barrier",
-	inventory_image = "barrier_barrier.png",
+    description = "Barrier",
+    inventory_image = "barrier_barrier.png",
     liquids_pointable = true,
-	tool_capabilities = {
-		full_punch_interval = 0.9,
-		max_drop_level=3,
-		groupcaps={
-			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=30, maxlevel=3},
-		},
-	},
+    tool_capabilities = {
+    full_punch_interval = 0.9,
+    max_drop_level=3,
+    groupcaps={
+        cracky = {times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=30, maxlevel=3},
+    },
+    },
     pointabilities = {
-        nodes = {
-            ["barrier:barrier"] = true,
-        }
+    nodes = {
+        ["barrier:barrier"] = true,
+    }
     },
     on_place = function(itemstack, placer, pointed_thing)
         -- if the pointed_thing is not a node, return the itemstack
@@ -49,15 +49,15 @@ minetest.register_tool("barrier:barrier_item", {
         end
         minetest.set_node(pos, {name = "barrier:barrier"})
         minetest.add_particle({
-			pos = pos,
-			velocity = {x = 0, y = 0, z = 0},
-			acceleration = {x = 0, y = 0, z = 0},
-			expirationtime = 1.05,
-			size = 8,
-			collisiondetection = false,
-			vertical = false,
-			texture = "barrier_barrier.png",
-			glow = 5,
+            pos = pos,
+            velocity = {x = 0, y = 0, z = 0},
+            acceleration = {x = 0, y = 0, z = 0},
+            expirationtime = 1.05,
+            size = 8,
+            collisiondetection = false,
+            vertical = false,
+            texture = "barrier_barrier.png",
+            glow = 5,
             playername = placer:get_player_name()
         })
         return itemstack
@@ -65,21 +65,21 @@ minetest.register_tool("barrier:barrier_item", {
 })
 
 local function show_barriers(nodes, player)
-	if not nodes or #nodes == 0 then return end
-	for n = 1, #nodes do
-		minetest.add_particle({
-			pos = nodes[n],
-			velocity = {x = 0, y = 0, z = 0},
-			acceleration = {x = 0, y = 0, z = 0},
-			expirationtime = 1.05,
-			size = 8,
-			collisiondetection = false,
-			vertical = false,
-			texture = "barrier_barrier.png",
-			glow = 5,
+    if not nodes or #nodes == 0 then return end
+    for n = 1, #nodes do
+        minetest.add_particle({
+            pos = nodes[n],
+            velocity = {x = 0, y = 0, z = 0},
+            acceleration = {x = 0, y = 0, z = 0},
+            expirationtime = 1.05,
+            size = 8,
+            collisiondetection = false,
+            vertical = false,
+            texture = "barrier_barrier.png",
+            glow = 5,
             playername = player:get_player_name()
         })
-	end
+    end
 end
 
 local function cyclic_update()
